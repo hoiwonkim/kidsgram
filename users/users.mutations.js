25276d18c3cf7a3d7154c274c0f7a5744d3ac3c7
Hoi Won KIM <gw31513@gmail.com>
2023-07-12 12:19:49 +1200

    2.4 2.5 Login

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import client from "../client";
-
 export default {
   Mutation: {
     createAccount: async (
@@ -21,7 +21,7 @@ export default {
           },
         });
         if (existingUser) {
-          throw new Error("This username/email is already taken.");
+          throw new Error("This username/password is already taken.");
         }
         const uglyPassword = await bcrypt.hash(password, 10);
         return client.user.create({
@@ -36,6 +36,27 @@ export default {
       } catch (e) {
         return e;
       }
+    },
+    login: async (_, { username, password }) => {
+      const user = await client.user.findFirst({ where: { username } });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password.",
+        };
+      }
+      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
--- users/users.typeDefs.js
+++ users/users.typeDefs.js
@@ -1,5 +1,4 @@
 import { gql } from "apollo-server";
-
 export default gql`
   type User {
     id: String!
@@ -10,6 +9,11 @@ export default gql`
     createdAt: String!
     updatedAt: String!
   }
+  type LoginResult {
+    ok: Boolean!
+    token: String
+    error: String
+  }
   type Mutation {
     createAccount(
       firstName: String!
@@ -18,6 +22,7 @@ export default gql`
       email: String!
       password: String!
     ): User
+    login(username: String!, password: String!): LoginResult!
   }
   type Query {
     seeProfile(username: String!): User
