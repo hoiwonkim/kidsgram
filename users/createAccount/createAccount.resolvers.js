0a4331f3c2820b0657b3b2bd3d2a1d7126e85a9d
Hoi Won KIM <gw31513@gmail.com>
2023-07-12 12:39:51 +1200

    2.6Divid and Conquer

--- users/createAccount/createAccount.resolvers.js
+++ users/createAccount/createAccount.resolvers.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import client from "../client";
+
+import client from "../../client";
+
 export default {
   Mutation: {
     createAccount: async (
@@ -37,26 +38,6 @@ export default {
         return e;
       }
     },
-    login: async (_, { username, password }) => {
-      const user = await client.user.findFirst({ where: { username } });
-      if (!user) {
-        return {
-          ok: false,
-          error: "User not found.",
-        };
-      }
-      const passwordOk = await bcrypt.compare(password, user.password);
-      if (!passwordOk) {
-        return {
-          ok: false,
-          error: "Incorrect password.",
-        };
-      }
-      const token = await jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-      return {
-        ok: true,
-        token,
-      };
-    },
+    
   },
 };
